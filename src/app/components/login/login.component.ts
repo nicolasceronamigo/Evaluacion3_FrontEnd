32c13d2c0149ac7e79f9bca1601243d5bfd15cc9
Alexis Castillo <alexis.castillo20@inacapmail.cl>
2026-07-10 13:30:46 -0400

    Estructura de rutas, navbar, login funcional y seccion about terminada

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { Router } from '@angular/router'; // 1. Importamos el Router de Angular
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], 
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  usuario: string = '';
+  contrasena: string = '';
 
+  // 2. Inyectamos el servicio Router en el constructor para poder usarlo
+  constructor(private router: Router) {}
+
+  iniciarSesion() {
+    if (this.usuario === 'admin' && this.contrasena === '1234') {
+      // 3. Redirección real a la pantalla del formulario de Nicolás
+      this.router.navigate(['/formulario']);
+    } else {
+      alert('Credenciales incorrectas. Intenta con admin / 1234');
+    }
+  }
 }
