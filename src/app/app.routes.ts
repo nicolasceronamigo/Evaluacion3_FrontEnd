5b1826eb4396d55f47964e83dc9bf4efc509b965
Alexis Castillo <alexis.castillo20@inacapmail.cl>
2026-07-09 20:30:03 -0400

    Rutas definitivas y navbar funcional con bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'about', component: AboutComponent }
+];
