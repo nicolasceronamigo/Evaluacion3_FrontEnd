d84a347b3e8cddfd813fee4ff2e219731348096c
nicolasceronamigo <nicolasceronamigo@gmail.com>
2026-07-10 22:52:28 -0400

    Se crea el formulario con validaciones y almacenamiento en localStorage. También se configuran las rutas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { FormularioComponent } from './components/formulario/formulario.component';
+import { PrestamoComponent } from './components/formulario/formulario.component';
 import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'formulario', component: FormularioComponent },
+  { path: 'formulario', component: PrestamoComponent },
   { path: 'about', component: AboutComponent }
 ];
